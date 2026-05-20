97548804bb5297ea90d65e368fc2e777e1febe7a
kjdinesh26-create <kjdinesh26@gmail.com>
2026-05-20 23:56:05 +0530

    push

--- src/components/TextPressure.tsx
+++ src/components/TextPressure.tsx
@@ -234,7 +234,7 @@ const TextPressure = ({
         {chars.map((char, i) => (
           <span
             key={i}
-            ref={el => (spansRef.current[i] = el)}
+            ref={el => { spansRef.current[i] = el; }}
             data-char={char}
             style={{
               display: 'inline-block',
