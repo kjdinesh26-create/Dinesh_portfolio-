d567735063129325511bbdfdf17f5c4545305e8f
kjdinesh26-create <kjdinesh26@gmail.com>
2026-05-20 23:34:58 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ArrowUpRight, Github, ExternalLink, Sparkles } from "lucide-react";
+import { ArrowUpRight, GitHub, ExternalLink, Sparkles } from "lucide-react";
 
 gsap.registerPlugin(ScrollTrigger);
 
